daeddd6466698ae65c95079642600fdede277cfc
AmirhosseinMLK <www.malekzadeh.amirhossein@gmail.com>
2021-08-04 10:14:49 +0430

    csv 2 dictionary label function added

--- DataReader.py
+++ DataReader.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 csv_path = 'severstal-steel-defect-detection/train.csv'
 
@@ -10,5 +11,38 @@ def csv_reader( csv_path):
 
 
 
+def csv2labelDict( csv_list):
+
+    dict_lbl = {}
+    #Row -> image_name, class_id, mask_row_lenght_code
+    for row in csv_list:
+        img_name, class_id, encoded_pixel = row
+        class_id = int(class_id)
+        encoded_pixel = encoded_pixel.split(' ')
+        encoded_pixel = list(  map( int, encoded_pixel ))
+        encoded_pixel = np.array(encoded_pixel).reshape( (-1,2) ) #firrst col-> start pix , second col -> count pix
+
+        if dict_lbl.get( img_name ) is None:
+            dict_lbl[ img_name ] = ( [class_id], [encoded_pixel] )
+        
+        else:
+            classes_list, encodedPx_list = dict_lbl.get( img_name )
+            classes_list.append( class_id )
+            encodedPx_list.append( encoded_pixel )
+            dict_lbl[img_name] = (classes_list, encodedPx_list)
+    return dict_lbl
+
+
+
+
 csv_list = csv_reader( csv_path)
+dict_lbl = csv2labelDict(csv_list)
+
+
+    
+
+
+
+
+    
 m=0
