04b6812574564b443b46a6f3c8f51e7e82cbdac1
ark1375 <ark.blackhat@gmail.com>
2021-08-09 17:44:07 +0430

    Moved csv_reader and csv_to_dict to ssdd_label_convertor.py

--- DataReader.py
+++ DataReader.py
@@ -9,62 +9,6 @@ img_path = 'severstal-steel-defect-detection/train_images'
 from sys import getsizeof
 
 #______________________________________________________________________________________________________________________________________________
-#expalin:
-#   read csv file from path and return csv list
-#
-#arg:
-#   csv_path: path of csv label
-#
-#return:
-#   csv list: 
-#       col_0: image_name, col_1: Class_ID, col_2: Encoded_pixel
-#______________________________________________________________________________________________________________________________________________
-def csv_reader( csv_path):
-    with open( csv_path, newline='') as csvfile :
-            csv_iter = csv.reader( csvfile)
-            csv_file = list(csv_iter)
-            return csv_file[1:]  
-
-
-
-
-#______________________________________________________________________________________________________________________________________________
-#explain:
-#   ->Convert csv list to dictionay that each key is a image_name and value is a tupel contatin classes_list and Encodded_pixel_list
-#   ->'image_name': ([classes], [encoded_pilxes_array])
-#   ->class: an int number
-#   ->encoded_pilxes_array: an array with shape(n,2) that first col is start pixel and second col is count pix ( order is form top to down then lft to right)
-#
-#arg:
-#   csv_list: output of csv_reader
-#
-#return:
-#   dict label
-#______________________________________________________________________________________________________________________________________________
-def csv2labelDict( csv_list):
-
-    dict_lbl = {}
-    #Row -> image_name, class_id, mask_row_lenght_code
-    for row in csv_list:
-        img_name, class_id, encoded_pixel = row
-        class_id = int(class_id) - 1 #in csv class start from 1
-        encoded_pixel = encoded_pixel.split(' ')
-        encoded_pixel = list(  map( int, encoded_pixel ))
-        encoded_pixel = np.array(encoded_pixel).reshape( (-1,2) ) #firrst col-> start pix , second col -> count pix
-
-        if dict_lbl.get( img_name ) is None:
-            dict_lbl[ img_name ] = ( [class_id], [encoded_pixel] )
-        
-        else:
-            classes_list, encodedPx_list = dict_lbl.get( img_name )
-            classes_list.append( class_id )
-            encodedPx_list.append( encoded_pixel )
-            dict_lbl[img_name] = (classes_list, encodedPx_list)
-    return dict_lbl
-
-
-
-#______________________________________________________________________________________________________________________________________________
 #explain:
 #   get path of images and split into val and train images_name list
 #
