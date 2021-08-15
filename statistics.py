774956842ebbfaa5f2df8262f274a4425a309de0
ark1375 <ark.blackhat@gmail.com>
2021-08-15 17:42:12 +0430

    Added defect_pixels_hit

--- statistics.py
+++ statistics.py
@@ -33,17 +33,49 @@ def binary_hist(annonation_path, annonations_name=None):
     ax.bar_label(bar, label_type='center', color='w')
     plt.show()
 
-    
+
+def defect_pixels_hist(annonation_path , userPercent = False , img_size = (256 * 1600)):
+
+    annonation_names = os.listdir(annonation_path)
+
+    def add_to_array(inp_dic , cls , count):
+        if cls in inp_dic.keys():
+            inp_dic[cls] += count
+        
+        elif cls not in inp_dic.keys():
+            inp_dic[cls] = count
+
+    buckets = {}
+
+    for anot_name in annonation_names:
+        # print(os.path.join(annonation_path , anot_name))
+        annotation = Annotation(os.path.join(annonation_path , anot_name))
+        if annotation.have_object() :
+            mask_list = annotation.get_encoded_mask()
+            for mask in mask_list:
+                count = np.sum( mask.codedMask_[:, 1] )
+                add_to_array(buckets , mask.class_ , count) 
+
+    print(buckets)
+    if userPercent:
+        total = sum(buckets.values())
+        print(total)
+        for key , elm in buckets.items():
+            buckets[key] /= (total * 0.01)
 
 
+    plt.bar(buckets.keys() , buckets.values())
+
+    plt.xlabel('Class')
+    plt.ylabel('Pix Number')
+    plt.title('Class-Pixel Bar Chart')
+    plt.show()
 
 
 
 
 csv_path = 'severstal-steel-defect-detection/train.csv'
 img_path = 'severstal-steel-defect-detection/train_images'
-annonation_path = 'severstal-steel-defect-detection/annotations'
-binary_hist(annonation_path=annonation_path, annonations_name= None)
-
-
-stop
+annonation_path = 'severstal-steel-defect-detection/annotations/'
+# binary_hist(annonation_path=annonation_path, annonations_name= None)
+defect_pixels_hist(annonation_path , userPercent=True)
