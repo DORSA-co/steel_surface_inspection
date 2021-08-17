ff367b111b9022bc300846ae4a6027c849355f2f
ark1375 <ark.blackhat@gmail.com>
2021-08-17 13:30:55 +0430

    Initializing model_develop, added config sample and main module

---
+++ model_config/ModelDeveloper.py
@@ -0,0 +1,32 @@
+import cv2
+import tensorflow
+form tensorflow import keras
+
+### Rename any method name as you fit
+
+class ModelInitializer():
+    
+    def __read__(path):
+        pass
+
+    def __init__():
+        pass
+
+    def __parse_conf(path):
+        pass
+
+    def cratore():
+        pass
+    
+    def dense2dense( input_shape , output_neuron, output_type ):
+        pass
+
+    def cnn2dense( input_shape , output_neuron , output_type ):
+        pass
+
+    def cnn2cnn( input_shape , output_neuron , output_type ):
+        pass
+
+
+
+
