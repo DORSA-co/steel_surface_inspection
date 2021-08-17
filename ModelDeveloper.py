58188b4ef25d326c4d4d1abd08d6a4ee759fc214
AmirhosseinMLK <www.malekzadeh.amirhossein@gmail.com>
2021-08-17 15:15:31 +0430

    functions_comented

--- ModelDeveloper.py
+++ ModelDeveloper.py
@@ -5,7 +5,7 @@ from tensorflow.python.keras.engine import keras_tensor
 
 ### Rename any method name as you fit
 BINARY = 'sigmoid'
-CLASSIFICATiON = 'softmax'
+CLASSIFICATION = 'softmax'
 POSETIVE_REGRESSION = 'relu'
 REGRESSION = None
 NORMAL_REGRESSION = 'tanh'
@@ -23,10 +23,23 @@ class ModelInitializer():
     def creator(self):
         pass
     
-    def dense2dense(self ,  input_shape , output_neuron, output_type ):
+    def simple_dense2dense(self ,  input_shape , output_neuron, output_type ):
         pass
 
-    def cnn2dense(self, input_shape , output_neuron , output_type ):
+    
+    #______________________________________________________________________________________________________________________________________________
+    #explain:
+    #   build a model coresponds to args
+    #arg:
+    #   input_shape, output_neuron, output_type
+    #   input_shape: shape of inputs in tuple format. the shape should be (h,w,channle)
+    #   output_neuron: numbrer of dense neuron in last layer. it show numner of output
+    #   output_type: type of outbut that could be BINARY, CLASSIFICATION, REGRESSION and so on
+    #
+    #return:
+    #   model
+    #______________________________________________________________________________________________________________________________________________
+    def simple_cnn2dense(self, input_shape , output_neuron , output_type ):
         model = keras.Sequential()
         model.add( keras.layers.Input(shape=input_shape))
         model.add( keras.layers.Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu'))
@@ -44,7 +57,19 @@ class ModelInitializer():
         return model
 
 
-    def cnn2cnn( self, input_shape , output_neuron , output_type ):
+    #______________________________________________________________________________________________________________________________________________
+    #explain:
+    #   build a model coresponds to args
+    #arg:
+    #   input_shape, output_neuron, output_type
+    #   input_shape: shape of inputs in tuple format. the shape should be (h,w,channle)
+    #   output_neuron: numbrer of channel in last layer. it show numner of output for each pixel in output array
+    #   output_type: type of outbut that could be BINARY, CLASSIFICATION, REGRESSION and so on
+    #
+    #return:
+    #   model
+    #______________________________________________________________________________________________________________________________________________
+    def simple_cnn2cnn( self, input_shape , output_neuron , output_type ):
         model = keras.Sequential()
         model.add( keras.layers.Input(shape=input_shape))
         model.add( keras.layers.Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu'))
