08822cc1448a9c5841056766cf395bd7a491ac20
ark1375 <ark.blackhat@gmail.com>
2021-08-17 13:34:23 +0430

    Minor name changes

--- model_config/ModelDeveloper.py
+++ model_config/ModelDeveloper.py
@@ -1,30 +1,30 @@
 import cv2
 import tensorflow
-form tensorflow import keras
+from tensorflow import keras
 
 ### Rename any method name as you fit
 
 class ModelInitializer():
     
-    def __read__(path):
+    def __read__(self , path):
         pass
 
-    def __init__():
+    def __init__(self):
         pass
 
-    def __parse_conf(path):
+    def __parse_conf(self , path):
         pass
 
-    def cratore():
+    def creator(self):
         pass
     
-    def dense2dense( input_shape , output_neuron, output_type ):
+    def dense2dense(self ,  input_shape , output_neuron, output_type ):
         pass
 
-    def cnn2dense( input_shape , output_neuron , output_type ):
+    def cnn2dense(self, input_shape , output_neuron , output_type ):
         pass
 
-    def cnn2cnn( input_shape , output_neuron , output_type ):
+    def cnn2cnn( self, input_shape , output_neuron , output_type ):
         pass
 
 
