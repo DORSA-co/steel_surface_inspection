2afe4632f5278b89af48d5cfd976ef57e8a26864
ReyhaneAbtahi <79043345+ReyhaneAbtahi@users.noreply.github.com>
2021-08-19 19:17:16 +0430

    new commit

--- LBP.py
+++ LBP.py
@@ -1,10 +1,10 @@
 import math
-
 from cv2 import *
 import numpy as np
 import timeit
 from skimage import feature
 from multiprocessing import Process, Queue
+
 
 def get_lbp(image, P, R, method, queue):
     # compute the Local Binary Pattern representation
@@ -27,7 +27,7 @@ def get_lbp(image, P, R, method, queue):
     # normalize weight between [0, 1]
     weight = (weight - np.min(weight)) / np.ptp(weight)
     c = 0
-    if method=='uniform':
+    if method == 'uniform':
         bins = P + 2
     else:
         bins = int(math.pow(2, P))
@@ -35,9 +35,9 @@ def get_lbp(image, P, R, method, queue):
     # calculate lbp histogram for each subimage if it's weight is more than average of weights
     for i in range(dimx, h + 1, dimx):
         for j in range(dimy, w + 1, dimy):
-            if weight[c] >= np.average(weight):
-                lbp = feature.local_binary_pattern(image[i-dimx:i, j-dimy:j], P, R, method)
-                h, _ = np.histogram(lbp.ravel(), density=False, bins=np.arange(0, bins+1), range=(0, bins))
+            if weight[c] >= 0.85:
+                lbp = feature.local_binary_pattern(image[i - dimx:i, j - dimy:j], P, R, method)
+                h, _ = np.histogram(lbp.ravel(), density=False, bins=np.arange(0, bins + 1), range=(0, bins))
                 hist = np.add(hist, h)
             c += 1
     queue.put({
@@ -50,17 +50,25 @@ def multiPreocessing(image, P, R, method, num_processes):
     height, width = image.shape
     processes = []
     queue = Queue()
-    segment_height = int(np.floor(height / num_processes))
-    for process_id in range(num_processes):
-        left_bound = process_id * segment_height
-        right_bound = (process_id * segment_height) + segment_height
-        if process_id == (num_processes - 1):
+    n = math.floor(math.sqrt(num_processes))
+    segment_height = int(np.floor(height / n))
+    segment_width = int(np.floor(width / n))
+    for i in range(n):
+        left_bound = i * segment_height
+        right_bound = (i * segment_height) + segment_height
+        if i == (num_processes - 1):
             # The last process should also process any remaining rows
             right_bound = height
-        I = image[left_bound:right_bound, ]
-        process = Process(target=get_lbp, args=(I, P, R, method, queue))
-        process.start()
-        processes.append(process)
+        for j in range(n):
+            up_bound = j * segment_width
+            down_bound = (j * segment_width) + segment_width
+            if j == (num_processes - 1):
+                # The last process should also process any remaining rows
+                down_bound = width
+            I = image[left_bound:right_bound, up_bound:down_bound]
+            process = Process(target=get_lbp, args=(I, P, R, method, queue))
+            process.start()
+            processes.append(process)
 
     # Wait for all processes to finish
     results = [queue.get() for process in processes]
@@ -68,20 +76,19 @@ def multiPreocessing(image, P, R, method, num_processes):
 
     # Format the pixels correctly for the output function,
     # which expects a linear list of pixel values.
-    if method=='uniform':
+    if method == 'uniform':
         s = P + 2
     else:
         s = int(math.pow(2, P))
     hist = np.zeros(s).astype('int64')
     for result in results:
         hist = np.add(hist, result['patterns'])
-        #hist = hist + result['patterns']
+        # hist = hist + result['patterns']
     return hist
+
 
 image = imread('/home/reyhane/Desktop/b.jpg', 0)
 start_time = timeit.default_timer()
-hist = multiPreocessing(image, 8, 1, 'uniform', 8)
+hist = multiPreocessing(image, 4, 1, 'uniform', 9)
 end_time = timeit.default_timer()
 print(end_time - start_time)
-
-
