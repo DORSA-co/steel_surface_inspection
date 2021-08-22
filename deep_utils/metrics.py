2ce542f1df3060713dd6d2828cc641b86b77ac3a
ark1375 <ark.blackhat@gmail.com>
2021-08-22 14:27:09 +0430

    Fixing division by zero and adding ratio to positive and negative

--- deep_utils/metrics.py
+++ deep_utils/metrics.py
@@ -4,13 +4,15 @@ import tensorflow.keras as keras
 import tensorflow.keras.metrics as metrics
 import numpy as np
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 class BIN_Metrics():
 
-    def __init__(self, threshold = 0.8):
+    def __init__(self, threshold = 0.5):
         self.__threshold = threshold
 
-    def BIN_TruePos(self , y_true, y_pred):
+    def True_Pos(self , y_true, y_pred):
 
         pred = tf.math.floor(
             (tf.math.sign( y_pred - self.__threshold ) + 1) / 2
@@ -20,12 +22,23 @@ class BIN_Metrics():
         pred = tf.cast(pred , tf.bool)
 
         metered = tf.logical_and(true , pred)
+        metered = tf.cast(metered , tf.float16)
 
-        metered = tf.cast(metered , tf.int32)
-        return tf.reduce_sum(metered)
+        all_true = ~ tf.math.logical_xor(true , pred)
+        all_true = tf.cast(all_true , tf.float16)
+
+        true_pos_count = tf.reduce_sum(metered)
+        all_count = tf.reduce_sum(all_true)
+
+        return tf.math.divide_no_nan(true_pos_count , all_count)
+
+                
 
 
-    def BIN_TrueNeg(self , y_true, y_pred):
+
+
+
+    def True_Neg(self , y_true, y_pred):
 
         pred = tf.math.floor(
             (tf.math.sign( y_pred - self.__threshold ) + 1) / 2
@@ -35,12 +48,20 @@ class BIN_Metrics():
         pred = tf.cast(pred , tf.bool)
 
         metered = tf.logical_and(~true , ~pred)
+        metered = tf.cast(metered , tf.float16)
 
-        metered = tf.cast(metered , tf.int32)
-        return tf.reduce_sum(metered)
+        all_true = ~ tf.math.logical_xor(true , pred)
+        all_true = tf.cast(all_true , tf.float16)
+
+        true_neg_count = tf.reduce_sum(metered)
+        all_count = tf.reduce_sum(all_true)
+
+        return tf.math.divide_no_nan(true_neg_count , all_count)
 
 
-    def BIN_FalsePos(self , y_true, y_pred ):
+
+
+    def False_Pos(self , y_true, y_pred ):
 
         pred = tf.math.floor(
             (tf.math.sign( y_pred - self.__threshold ) + 1) / 2
@@ -50,12 +71,20 @@ class BIN_Metrics():
         pred = tf.cast(pred , tf.bool)
 
         metered = tf.logical_and(~true , pred)
+        metered = tf.cast(metered , tf.float16)
 
-        metered = tf.cast(metered , tf.int32)
-        return tf.reduce_sum(metered)
+        all_false = tf.math.logical_xor(true , pred)
+        all_false = tf.cast(all_false , tf.float16)
+
+        false_pos_count = tf.reduce_sum(metered)
+        all_count = tf.reduce_sum(all_false)
+
+        return tf.math.divide_no_nan(false_pos_count , all_count)
+
+
         
 
-    def BIN_FalseNeg(self , y_true, y_pred):
+    def False_Neg(self , y_true, y_pred):
 
         pred = tf.math.floor(
             (tf.math.sign( y_pred - self.__threshold ) + 1) / 2
@@ -65,22 +94,57 @@ class BIN_Metrics():
         pred = tf.cast(pred , tf.bool)
 
         metered = tf.logical_and(true , ~pred)
-        metered = tf.cast(metered , tf.int32)
+        metered = tf.cast(metered , tf.float16)
+        
+        all_false = tf.math.logical_xor(true , pred)
+        all_false = tf.cast(all_false , tf.float16)
 
-        return tf.reduce_sum(metered)
+        false_neg_count = tf.reduce_sum(metered)
+        all_count = tf.reduce_sum(all_false)
+       
+
+        return tf.math.divide_no_nan(false_neg_count , all_count)
+        
 
 
-# yt = [[0, 0, 0, 0],[1,1,0,0],[0,  0,0,  0],[1,   0,0,1]]
-# yp = [[1, 1, 0, 1],[0,1,0,0],[0.9,0,0.2,0],[0.88,0,0,0]]
-# yt = tf.constant(yt)
-# yp = tf.constant(yp)
+'''
+data_ = np.random.rand( 100, 8 )
+print(data_)
+label_ = np.sum(data_ , axis= 1)
+print(label_)
+model = Sequential()
+model.add(Dense(20 , input_dim=8 , activation='relu'))
+model.add(Dense(20 , activation='relu'))
+model.add(Dense(20 , activation='relu'))
+model.add(Dense(1 , activation='sigmoid'))
 
-# binm = BIN_Metrics()
-# print(binm.BIN_FalseNeg( yt , yp))
-# print(binm.BIN_FalsePos(yt , yp))
-# print(binm.BIN_TrueNeg(yt , yp))
-# print(binm.BIN_TruePos(yt , yp))
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=[
+        'accuracy' ,
+        BIN_Metrics(0.7).False_Neg ,
+        BIN_Metrics(0.7).False_Pos ,
+        BIN_Metrics(0.7).True_Neg ,
+        BIN_Metrics(0.7).True_Pos ,
+        ])
+'''
 
+
+'''
+model.fit(data_, label_, epochs=10, batch_size=1)
+
+yt = [[0, 0, 0, 0],[1,1,0,0],[0,  0,0,  0],[1,   0,0,1]]
+yp = [[1, 1, 0, 1],[0,1,0,0],[0.9,0,0.2,0],[0.88,0,0,0]]
+yt = tf.constant(yt)
+yp = tf.constant(yp)
+
+binm = BIN_Metrics()
+print(binm.False_Neg( yt , yp))
+print(binm.False_Pos(yt , yp))
+print(binm.True_Neg(yt , yp))
+print(binm.True_Pos(yt , yp))
+'''
 
 
 
